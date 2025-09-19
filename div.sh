d90395ecda422a7571b89c413cd290ae5418976d
Ubuntu <ubuntu@ip-172-31-0-253.eu-north-1.compute.internal>
2025-09-19 07:40:19 +0000

    this is div file
    ^x
    ^X

---
+++ div.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Multiplication of 2 numbers
+
+# Take input from user
+echo "Enter first number: "
+read num1
+
+echo "Enter second number: "
+read num2
+
+# Perform multiplication
+product=$((num1 / num2))
+
+# Display result
+echo "The product of $num1 and $num2 is: $product"
+
