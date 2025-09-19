41da73299c334d2c593fbd7df16ef5bcb35bbcf7
Ubuntu <ubuntu@ip-172-31-0-253.eu-north-1.compute.internal>
2025-09-19 06:02:42 +0000

    add

---
+++ add.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Simple addition script
+
+# Take input from user
+echo "Enter first number: "
+read num1
+
+echo "Enter second number: "
+read num2
+
+# Perform addition
+sum=$((num1 + num2))
+
+# Display result
+echo "The sum of $num1 and $num2 is: $sum"
+
